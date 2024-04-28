051c43465c70ae0add2a3a4969d6d55ed4724b50
Juanes7222 <133039607+Juanes7222@users.noreply.github.com>
2024-04-27 19:19:44 -0500

    Update Relationaspect.py

--- Relationaspect.py
+++ Relationaspect.py
@@ -1,97 +1,173 @@
-from PIL import Image
-from torchvision import models, transforms
+from torchvision.io import read_image
+from torchvision.utils import make_grid, draw_bounding_boxes
+from torchvision.models.detection import fasterrcnn_resnet50_fpn, FasterRCNN_ResNet50_FPN_Weights
 from matplotlib import pyplot as plt
+from pathlib import Path
+import torchvision.transforms.functional as F
 import cv2
 import numpy as np
-import torch
+import os
+import argparse
 
-# Cargar el modelo preentrenado
-model = models.detection.fasterrcnn_resnet50_fpn(weights=models.detection.FasterRCNN_ResNet50_FPN_Weights.DEFAULT)
-model.eval() # Cambiar el modelo a modo de evaluación
+plt.rcParams["savefig.bbox"] = 'tight'
 
-def preprocesar_imagen(imagen_path):
-    # Definir las transformaciones
-    transform = transforms.Compose([
-        transforms.Resize((800, 800)), # Cambiar el tamaño de la imagen
-        transforms.ToTensor(), # Convertir la imagen a tensor
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]), # Normalizar
-    ])
-    
-    # Cargar y preprocesar la imagen
-    imagen = Image.open(imagen_path).convert("RGB")
-    imagen_tensor = transform(imagen).unsqueeze(0) # Añadir una dimensión extra para el batch
-    
-    return imagen_tensor
+def list_dir_images(path):
+    files = os.listdir(path)
+    types = ["jpg", "png"]
+    images = [file for file in files if file.split(".")[-1] in types]
+    return images
 
-# Uso de la función
-imagen_tensor = preprocesar_imagen('./DSC_0471.jpg')
+def prepare_images(images):
+    images_list = [read_image(image) for image in images]
+    return images_list
 
-def detectar_personas(imagen_tensor, modelo):
-    # Realizar la detección
-    detecciones = modelo(imagen_tensor)
-    
-    # Extraer las cajas de detección y las etiquetas
-    cajas = detecciones[0]['boxes']
-    etiquetas = detecciones[0]['labels']
-    
-    # Filtrar solo las detecciones de personas
-    personas = [caja for caja, etiqueta in zip(cajas, etiquetas) if etiqueta == 10] # Asumiendo que la etiqueta 0 es "persona"
-    
-    return personas
+def show(imgs):
+    if not isinstance(imgs, list):
+        imgs = [imgs]
+    fig, axs = plt.subplots(ncols=len(imgs), squeeze=False)
+    for i, img in enumerate(imgs):
+        img = img.detach()
+        img = F.to_pil_image(img)
+        axs[0, i].imshow(np.asarray(img))
+        axs[0, i].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
 
-# Uso de la función
-# personas = detectar_personas(imagen_tensor, model)
+# grid = make_grid(images_list)
+# show(grid)
 
 
-# def visualizar_detecciones(imagen_path, personas):
-#     imagen = Image.open(imagen_path)
-#     fig, ax = plt.subplots(1)
-#     ax.imshow(imagen)
-    
-#     for caja in personas:
-#          np_box = caja.detach().numpy()
-#          x1, y1, x2, y2 = np_box
-#          rect = plt.Rectangle((x1, y1), x2 - x1, y2 - y1, fill=False, color='red')
-#          ax.add_patch(rect)
-    
-#     plt.show()
+# persons_with_boxes = [
+#     draw_bounding_boxes(image_int, boxes=output['boxes'][output['scores'] > score_threshold], width=8)
+#     for image_int, output in zip(images_list, outputs)
+# ]
+# print(persons)
+# show(persons_with_boxes)
 
-def visualizar_detecciones(imagen_path, detecciones):
-    # Cargar la imagen
-    imagen = Image.open(imagen_path)
-    fig, ax = plt.subplots(1)
-    ax.imshow(imagen)
+def cut_with_aspect(image, aspect_relation, box):
+    # Relación de aspecto es una tupla (ancho, alto)
+    original_width, original_height = image.shape[1], image.shape[0]
+    desired_width, desired_height = aspect_relation
+
+    # Calcular el nuevo tamaño de la imagen manteniendo la relación de aspecto
+    if (original_width / original_height) > (desired_width / desired_height):
+        new_width = original_height * (desired_width / desired_height)
+        new_height = original_height
+    else:
+        new_width = original_width
+        new_height = original_width * (desired_height / desired_width)
+
+    # Calcular la posición de recorte
+    left = (original_width - new_width) / 2
+    up = (original_height - new_height) / 2
+    right = (original_width + new_width) / 2
+    down = (original_height + new_height) / 2
+    left, up, right, down = validate_size((left, up, right, down), box)
+    # Recortar la imagen
+
+    new_image = image[int(up):int(down), int(left):int(right)]
+
+    return new_image
+
+def select_box(persons):
+    box = []
+    name_index = ["min", "min", "max", "max"]
+    for j in range(4):
+        value = 0
+        if name_index[j] == "min":
+            for i in range(4):
+                person_value = persons[i][j]
+                if value > person_value:
+                    value = person_value
+        else:
+            for i in range(4):
+                person_value = persons[i][j]
+                if value < person_value:
+                    value = person_value
+        box.append(value)
+    print(box)
+    return box
+
+def validate_size(sizes: tuple, box):
+    left, up, right, down = sizes
+    xmin, ymin, xmax, ymax = box
+    if xmin > right or xmax < left or ymin > down or ymax < up:
+        # Si la persona está fuera del área de recorte, no se hace nada
+        return sizes
+    if xmin < left:
+        left = xmin - 300
+    if xmax > right:
+        right = xmax + 300
+    if ymin < up:  
+        up = ymin - 300
+    if ymax > down:
+        down = ymax + 300
+    sizes = (left, up, right, down)
+    return sizes
+
+def prepare_model(weights, progress=False):
+    model = fasterrcnn_resnet50_fpn(weights=weights, progress=progress)
+    model = model.eval()
+    return model
+
+def predict(images, model):
+
+    outputs = model(images)
     
-    # Extraer las cajas de detección y las etiquetas
-    cajas = detecciones[0]['boxes'].detach().numpy()
-    etiquetas = detecciones[0]['labels'].detach().numpy()
+    return outputs
+
+def transform_images(images_list, weights):
+    transforms = weights.transforms()
+
+    images = [transforms(d) for d in images_list]
     
-    # Definir un diccionario para mapear las etiquetas a nombres de clases
-    # Este paso es opcional y depende de cómo estén etiquetadas tus clases
-    # En este ejemplo, asumimos que las etiquetas son índices numéricos
-    # que corresponden a clases predefinidas.
-    # Ajusta este diccionario según tus necesidades.
+    return images
+
+def select_persons(outputs):
+    score_threshold = .9
+    persons = [output['boxes'][output['scores'] > score_threshold].detach().numpy() for output in outputs]
     
-    for caja, etiqueta in zip(cajas, etiquetas):
-        x1, y1, x2, y2 = caja
-        rect = plt.Rectangle((x1, y1), x2 - x1, y2 - y1, fill=False, color='red')
-        ax.add_patch(rect)
+    return persons
+
+def save_image(image, name):
+    cv2.imwrite(name, image)
+    
+def select_relation(image):
+    original_width, original_height = image.shape[1], image.shape[0]
+    if original_height < original_width:
+        return (16, 9)
+    return (1, 1)
+
+def create_name(path: Path):
+    base_dir = path.parent
+    name = path.name
+    name = name.replace(".jpg", "_cut.jpg")
+    return base_dir/name
+
+def main(path):
+    list_dir = list_dir_images(path)
+    images_list = prepare_images(list_dir)
+    weight = FasterRCNN_ResNet50_FPN_Weights.DEFAULT
+    transforms_images = transform_images(images_list, weight)
+    model = prepare_model(weight)
+    outputs = predict(transforms_images, model)
+    persons_selected = select_persons(outputs)
+    for i, original_image in enumerate(list_dir):
+        opened_image = cv2.imread(original_image)
+        box = select_box(persons_selected[i])
+        new_image = cut_with_aspect(opened_image, box)
+        path = Path(original_image)
+        name_image = create_name(path)
+        save_image(new_image, name_image.as_posix())
         
-        # Añadir la etiqueta de la clase en la esquina superior izquierda de la caja
-        ax.text(x1, y1, etiqueta, fontsize=10, bbox=dict(facecolor='white', alpha=0.7))
-    plt.show()
-
-# Uso de la función
-visualizar_detecciones('./DSC_0471.jpg', model(imagen_tensor))
-
-
-def change_relation(image: Image, relation: str):
-   new_width, new_height = relation.split(":")
-   width, height = image.size
-   
-   factor_scale = min(width/new_width, height/new_height)
-   
-   width_cut = width/factor_scale
-   height_cut = height/factor_scale
-   
-   
+def check_params():
+    parser = argparse.ArgumentParser(description='Revisar parámetros de línea de comandos')
+    
+    parser.add_argument("-p", "--path", type=str, default="./", required=True, help="Determine the path of images")
+    
+    args = parser.parse_args()
+    main(**vars(args))
+    
+if "__main__" == __name__:
+    # check_params()
+    main("./")
+        
+    
