88f93d07b4e8c857a4d210aac2b98758a1df9573
Juanes7222 <133039607+Juanes7222@users.noreply.github.com>
2024-12-02 19:31:53 -0500

    Create DownloadYoutube.py

---
+++ DownloadYoutube.py
@@ -0,0 +1,22 @@
+import os
+import yt_dlp    
+
+def descargar_audio_mp3_2(url, output_path="descargas"):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '320',
+        }],
+        'outtmpl': f'{output_path}/%(title)s.%(ext)s',
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+
+
+videos = ["https://youtu.be/K3z836Qv8ek"]
+
+for video in videos:
+    descargar_audio_mp3_2(video, r"C:\Users\juanb\Downloads\Musica")
